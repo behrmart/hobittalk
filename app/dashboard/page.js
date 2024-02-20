054f17ae298d4305d41adf82a281cc3fb3635037
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2024-02-20 17:26:39 -0600

    added dashboard routing page

---
+++ app/dashboard/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h2>Dashboard Page app/dashboard/page.js</h2>
+        <p> app/dashboard/page.js Dashboard</p>
+      </div>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <div>
-        <h1>Hobbit Talk app/page.js</h1>
+        <h1>Next.JS app/page.js</h1>
         <p> app/page.js of Hobbit Talk project</p>
       </div>
     </main>
