f4de3fe5d2cb2a1df4f7d12be3d4c8d5f86c0ecb
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2024-02-20 16:39:34 -0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}></div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}></div>
+    </main>
+  );
+}
