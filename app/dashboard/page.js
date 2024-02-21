35001601bf3cf68b6a67285b6433d73cbc16d97e
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2024-02-21 11:56:08 -0600

    removed tailwind installed chakra UI

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <div>
         <h2>Dashboard Page app/dashboard/page.js</h2>
         <p> app/dashboard/page.js Dashboard</p>
+        <Link href="/">Home</Link>
       </div>
     </main>
   );
--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <div>
-        <h1>Next.JS app/page.js</h1>
+        <h1>Hobbit Talk</h1>
         <p> app/page.js of Hobbit Talk project</p>
       </div>
+      <Link href="/dashboard" color="blue.400" _hover={{ color: "blue.500" }}>
+        Dashboard
+      </Link>
     </main>
   );
 }
